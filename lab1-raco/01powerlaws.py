7e83ff7c7cd4c878cbe5363814fcaf39e87c0272
AdrianHagen <adrian.hagen1@icloud.com>
2025-09-17 17:05:21 +0200

    initial python script code

--- lab1-raco/01powerlaws.py
+++ lab1-raco/01powerlaws.py
@@ -1,1 +1,28 @@
 # Utils script for the first exercise
+import nltk
+import pprint
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+from nltk.stem import SnowballStemmer
+import matplotlib.pyplot as plt
+
+import urllib.request
+
+target_url = (
+    "https://fegalaz.usc.es/~gamallo/aulas/lingcomputacional/corpus/quijote-en.txt"
+)
+
+quijote_text = urllib.request.urlopen(target_url)
+
+# tokenized with no pre process
+tokenized_text = word_tokenize(quijote_text)
+
+# remove stopwords
+english_sw = set(stopwords.words("english") + list(string.punctuation))
+
+filtered_tokenized_text = [
+    w.lower() for w in tokenized_text if w.lower() not in english_sw
+]
+
+pprint.pprint(filtered_tokenized_text)
